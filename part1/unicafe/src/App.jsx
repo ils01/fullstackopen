b6267b427dd0079373f925798e07778647bc8729
ils01 <theilyashulepov@gmail.com>
2024-01-05 13:38:55 +0300

    part 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,17 +5,47 @@ function App() {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const [all, setAll] = useState(0);
+    const [average, setAverage] = useState(0);
+    const [positive, setPositive] = useState(0);
+    let newGood = good;
+    let newNeutral = neutral;
+    let newBad = bad;
+    let newAll = all;
+    let newAvg = average;
+    let newPos = positive;
 
+    function updateAll() {
+        newAll = (newGood || 0) + (newNeutral || 0) + (newBad || 0);
+        setAll(newAll);
+    }
+    function updateAvg() {
+        newAvg = ((newGood || 0) - (newBad || 0)) / newAll;
+        setAverage(newAvg)
+    }
+    function updatePos() {
+        console.log('newgood', newGood)
+        newPos = (newGood || 0) / (newAll) * 100
+        setPositive(newPos)
+    }
+    function upd() {
+        updateAll();
+        updateAvg();
+        updatePos();
+    }
     function addGood() {
-        let goodPlusOne = good + 1;
-        setGood(goodPlusOne);
+        newGood = good + 1;
+        setGood(newGood);
+        upd();
     }
     function addNeutral() {
-        let neutralPlusOne = neutral + 1;
-        setNeutral(neutralPlusOne);
+        newNeutral = neutral + 1;
+        setNeutral(newNeutral);
+        upd();
     } function addBad() {
-        let badPlusOne = bad + 1;
-        setBad(badPlusOne);
+        newBad = bad + 1;
+        setBad(newBad);
+        upd();
     }
     return (
         <div>
@@ -30,6 +60,10 @@ function App() {
                 <p>good {good}</p>
                 <p>neutral {neutral}</p>
                 <p>bad {bad}</p>
+                <hr />
+                <p>all {all}</p>
+                <p>average {average}</p>
+                <p>positive {positive} %</p>
             </div>
         </div>
     )
